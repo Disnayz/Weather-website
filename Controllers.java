822b7699a980ce605a60685aeec13c3fc89f497f
Disnayz <159566429+Disnayz@users.noreply.github.com>
2024-06-17 13:50:41 +0200

    Update Controllers.java

--- Controllers.java
+++ Controllers.java
@@ -1,6 +1,9 @@
 package com.adamd.website;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ui.Model;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +24,8 @@ public String searchForLocation(){
     @GetMapping("/CityData")
     public String showCityData(Model model){
         List<CityWeatherData> city = new ArrayList<CityWeatherData>();
-        return "SearchForLocation";
+        city.add(new CityWeatherData("Europe/Warsaw", "5:45", "22:30", 279.13, 276.44, 1029, 64, 0.06f, 2, 3));
+        System.err.println(city);
+        return "City.html";
     }
 }
