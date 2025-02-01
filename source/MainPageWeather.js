8701659c384a2b171fb6470e1ffea0e86b9c331a
bartohehe <bartosz.pski@gmail.com>
2025-02-01 15:32:18 +0100

    Naprawiona fukcja na stronie główniej i zmienione ikonki

--- source/MainPageWeather.js
+++ source/MainPageWeather.js
@@ -1,47 +1,65 @@
-const city = ["London", "New York", "Warsaw"];
+const cities = ["London", "New York", "Warsaw"];
 
-function showWeather(){
+const api_key = 'e23baa385d86a26dcc2c7311ae9fd17d'
+
+const allIcon = {
+    "01d": "source/assets/sun.png",
+    "01n": "source/assets/moon.png",
+    "02d": "source/assets/fewCloudsSun.png",
+    "02n": "source/assets/fewCloudsMoon.png",
+    "03d": "source/assets/cloud.png",
+    "03n": "source/assets/cloud.png",
+    "04d": "source/assets/brokenClouds.png",
+    "04n": "source/assets/brokenClouds.png",
+    "09d": "source/assets/showerRain.png",
+    "09n": "source/assets/showerRain.png",
+    "10d": "source/assets/rain.png",
+    "10n": "source/assets/rain.png",
+    "11d": "source/assets/thunderstorm.png",
+    "11n": "source/assets/thunderstorm.png",
+    "13d": "source/assets/snow.png",
+    "13n": "source/assets/snow.png",
+    "40d": "source/assets/mist.png",
+    "40n": "source/assets/mist.png",
+};
+
+function showWeather(city){
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
     .then(response => response.json())
     .then(data => {
         // Extracting the necessary data from the API response
+        const icon = allIcon[data.weather[0].icon]
         const weather = {
             location: data.name,
-            timezone: data.timezone,
-            sunrise: new Date(data.sys.sunrise * 1000).toLocaleTimeString(),
-            sunset: new Date(data.sys.sunset * 1000).toLocaleTimeString(),
             temp: Math.floor(data.main.temp),
             feelsLike: Math.floor(data.main.feels_like),
-            pressure: data.main.pressure,
             humidity: data.main.humidity,
-            clouds: data.clouds.all,
             windSpeed: data.wind.speed,
-            icon: data.weather.icon
         };
 
 
-        // Show the weather table
-        document.getElementById("weatherData").style.display = "block";
+        // Create div and populute it with weather data
+        const cityCard = document.createElement("div");
+          cityCard.classList.add("city-card");
 
-        // Populate the table with data
-        const table = document.getElementById("weatherTable");
-        table.innerHTML = `
-            <tr><th colspan='2' class='name'>${weather.location}</th></tr>
-            <tr><th>Field</th><th>Value</th></tr>
-            <tr><td>Timezone</td><td>${weather.timezone}</td></tr>
-            <tr><td>Sunrise</td><td>${weather.sunrise}</td></tr>
-            <tr><td>Sunset</td><td>${weather.sunset}</td></tr>
+          cityCard.innerHTML = `
+            <table class="weather-table" id="mainPageTable">
+            <tr><th colspan='2' class='name'>${weather.location}<img src=${icon} style="height: 75px;"></th></tr>
             <tr><td>Temperature</td><td>${weather.temp} °C</td></tr>
             <tr><td>Feels Like Temperature</td><td>${weather.feelsLike} °C</td></tr>
-            <tr><td>Pressure</td><td>${weather.pressure} hPa</td></tr>
             <tr><td>Humidity</td><td>${weather.humidity} %</td></tr>
-            <tr><td>Cloud Cover</td><td>${weather.clouds} %</td></tr>
             <tr><td>Wind Speed</td><td>${weather.windSpeed} m/s</td></tr>
-            `;
+            </table>
+          `;
+
+          // Append the city card to the container
+          document.getElementById("weather-tables-container").appendChild(cityCard);
     })
     .catch(error => {
         alert("Error fetching weather data: " + error);
     });
   };
 
-    city.forEach(showWeather());
+  cities.forEach(city => {
+    showWeather(city);
+  });
--- source/Weather.js
+++ source/Weather.js
@@ -1,14 +1,34 @@
 document.getElementById("searchForm").addEventListener("submit", function(event){
+    
     event.preventDefault();
     const city =document.getElementById("cityInput").value;
-
-    const api_key = 'e23baa385d86a26dcc2c7311ae9fd17d'
     
-
+    const api_key = 'e23baa385d86a26dcc2c7311ae9fd17d'
+    const allIcon = {
+        "01d": "source/assets/sun.png",
+        "01n": "source/assets/moon.png",
+        "02d": "source/assets/fewCloudsSun.png",
+        "02n": "source/assets/fewCloudsMoon.png",
+        "03d": "source/assets/cloud.png",
+        "03n": "source/assets/cloud.png",
+        "04d": "source/assets/brokenClouds.png",
+        "04n": "source/assets/brokenClouds.png",
+        "09d": "source/assets/showerRain.png",
+        "09n": "source/assets/showerRain.png",
+        "10d": "source/assets/rain.png",
+        "10n": "source/assets/rain.png",
+        "11d": "source/assets/thunderstorm.png",
+        "11n": "source/assets/thunderstorm.png",
+        "13d": "source/assets/snow.png",
+        "13n": "source/assets/snow.png",
+        "40d": "source/assets/mist.png",
+        "40n": "source/assets/mist.png",
+    };
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
     .then(response => response.json())
     .then(data => {
         // Extracting the necessary data from the API response
+        const icon = allIcon[data.weather[0].icon]
         const weather = {
             location: data.name,
             timezone: data.timezone,
@@ -20,18 +40,19 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
             humidity: data.main.humidity,
             clouds: data.clouds.all,
             windSpeed: data.wind.speed,
-            incon: data.weather.icon
         };
 
-
-        // Show the weather table
+       
+        
+        
+        // Show the weather table and hide main tables
         document.getElementById("weatherData").style.display = "block";
+        document.getElementById("weather-tables-container").style.display = "none";
 
         // Populate the table with data
         const table = document.getElementById("weatherTable");
         table.innerHTML = `
-            <tr><th colspan='2' class='name'>${weather.location}</th></tr>
-            <tr><th>Field</th><th>Value</th></tr>
+            <tr><th colspan='2' class='name'>${weather.location}<img src=${icon} style="height: 75px;" id="weather-icon"></th></tr>
             <tr><td>Timezone</td><td>${weather.timezone}</td></tr>
             <tr><td>Sunrise</td><td>${weather.sunrise}</td></tr>
             <tr><td>Sunset</td><td>${weather.sunset}</td></tr>
@@ -47,3 +68,4 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
         alert("Error fetching weather data: " + error);
     });
 });
+
